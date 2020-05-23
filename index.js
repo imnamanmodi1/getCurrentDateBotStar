fb36589bd4edf1abb84c1ce49f3f17f88b6c4331
Naman Modi <modi.naman14@gmail.com>
2020-05-23 06:29:47 +0530

    [NEW] - Includes Time in the script
    updated the code to include the time in human readable

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-//make a variable Name dateToday in your botStar flow before adding this script
+//make a variable Name dateToday & timeToday in your botStar flow before adding this script
 //making date object
 var date_ob = new Date();
 let date = ("0" + date_ob.getDate()).slice(-2);
 let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
 let year = date_ob.getFullYear();
+// getTime
+var h = date_ob.getHours();
+var m = date_ob.getMinutes();
+var s = date_ob.getSeconds();
 
+//finalTime
+var finalTime = h + ":" + m + ":" + s;
 //final date
 let todayDate = date + "/" + month + "/" + year;
 
@@ -14,6 +20,7 @@ done({
       type: "set_variable",
       data: {
         dateToday: todayDate,
+        timeToday: finalTime,
       },
     },
   ],
