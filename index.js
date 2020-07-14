342de8d0caff98f7d3266d96e615dc2f80248ca7
Naman Modi <modi.naman14@gmail.com>
2020-07-14 18:53:23 +0530

    Updated code to handle timezone offsets in code

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 //make a variable Name dateToday & timeToday in your botStar flow before adding this script, both variable data type should be Text
+
+//handling different timezone here, Add your UTC Offset here such as for London(1), for Paris(2) & so on
+var offset = 2;
+
 //making date object
-var date_ob = new Date();
+var d = new Date();
+var utc = d.getTime() + d.getTimezoneOffset() * 60000;
+var date_ob = new Date(utc + 3600000 * 2);
 let date = ("0" + date_ob.getDate()).slice(-2);
 let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
 let year = date_ob.getFullYear();
